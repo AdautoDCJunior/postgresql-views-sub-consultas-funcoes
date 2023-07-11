70d1df2d34e16fdc72551518a95a3b90c9b616eb
Adauto Donizete Junior - Business Intelligence Lev Negócios <analista.bi@levnegocios.com.br>
2023-07-11 20:32:43 -0300

    feat: finishing class 03

---
+++ sql-aula-3-completo.sql
@@ -0,0 +1,20 @@
+SELECT * FROM curso WHERE categoria_id = 1 OR categoria_id = 2;
+
+SELECT * FROM curso WHERE categoria_id IN (1,2);
+
+
+SELECT * FROM curso WHERE categoria_id IN (
+    SELECT id FROM categoria WHERE nome NOT LIKE ('% %')
+);
+
+
+SELECT categoria,
+    numero_cursos
+FROM (
+    SELECT categoria.nome AS categoria,
+        COUNT(curso.id) as numero_cursos
+    FROM categoria
+    JOIN curso ON curso.categoria_id = categoria.id
+    GROUP BY categoria
+) AS categoria_cursos
+WHERE numero_cursos > 3;
